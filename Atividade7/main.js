fc278e91ccdbcb05cbe125ee4938f3a3c3356f72
Matheus Candido <99218966+mathHenri05@users.noreply.github.com>
2023-09-13 17:22:45 -0300

    Update main.js

--- Atividade7/main.js
+++ Atividade7/main.js
@@ -1,36 +1,24 @@
-while(true){
-    jogador1 = prompt("Jogador 1: Digite pedra, papel ou tesoura");
-    jogador2 = prompt("Jogador 2: Digite pedra, papel ou tesoura");
-
-    if (jogador1 == "pedra") {
-        if (jogador2 == "tesoura") {
-             alert ("Pedra quebra tesoura");
-        } else if (jogador2 == "papel"){
-            alert ("Papel envolve a Pedra");
-        } else if (jogador2 == "pedra") {
-            alert ("Empate");
-        }
-
-        }
-    else if (jogador1 == "tesoura") {
-        if (jogador2 == "pedra") {
-            alert ("Pedra quebra tesoura");
-        } else if (jogador2 == papel){
-            alert ("tesoura corta papel");
-        } else if (jogadoe2 == "tesoura") {
-                alert ("Empate");
-        }
+while (true) {
+    var jogador1 = prompt("Jogador 1: Digite pedra, papel ou tesoura");
+    var jogador2 = prompt("Jogador 2: Digite pedra, papel ou tesoura");
+  
+    jogador1 = jogador1.toLowerCase(); // Converter para minúsculas para tornar a comparação não sensível a maiúsculas
+  
+    if (jogador1 === jogador2) {
+      alert("Empate");
+    } else if (
+      (jogador1 === "pedra" && jogador2 === "tesoura") ||
+      (jogador1 === "tesoura" && jogador2 === "papel") ||
+      (jogador1 === "papel" && jogador2 === "pedra")
+    ) {
+      alert("Jogador 1 vence!");
+    } else {
+      alert("Jogador 2 vence!");
     }
-
-    else if (jogador1 == "papel"){
-        if (jogador2 == "pedra") {
-            alert ("Papel envolve pedra");
-        }else if (jogador2 == "tesoura") {
-            alert ("Tesoura corta papel");
-        }else if (jogador2 == "papel")  {
-            alert ("Empate");
-        }
-
-        }
-
+  
+    var continuar = prompt("Deseja jogar novamente? (s/n)").toLowerCase();
+    if (continuar !== "s") {
+      break; 
     }
+  }
+  
