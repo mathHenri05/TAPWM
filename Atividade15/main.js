5022313c65ab29037cadfeda4141a70ba6e188a1
Matheus Candido <99218966+mathHenri05@users.noreply.github.com>
2023-11-29 15:28:20 -0300

    Update main.js

--- Atividade15/main.js
+++ Atividade15/main.js
@@ -1,17 +1,17 @@
 function inicio(){
     document.getElementById("texto").innerHTML = "Janela Fechada";
-    document.getElementById('imagem').src = "janela-fechada.jpeg";
+    document.getElementById('imagem').src = "src/janela-fechada.jpeg";
 }
 
 function abre(){
-    document.getElementById('imagem').src = "janela-aberta.jpeg";
+    document.getElementById('imagem').src = "src/janela-aberta.jpeg";
     document.getElementById("texto").innerHTML = "Janela aberta";
 }
 function fecha(){
-    document.getElementById('imagem').src = "fechada.jpeg";
+    document.getElementById('imagem').src = "src/fechada.jpeg";
     document.getElementById("texto").innerHTML = "Janela Fechada";
 }
 function quebra(){
-    document.getElementById('imagem').src = "janela-quebrada.jpeg";
+    document.getElementById('imagem').src = "src/janela-quebrada.jpeg";
     document.getElementById("texto").innerHTML = "Janela Quebrada";
 }
