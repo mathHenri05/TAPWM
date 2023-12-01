562294f3a2f672fce068b19bd9f8a3109038c982
Matheus Candido <99218966+mathHenri05@users.noreply.github.com>
2023-11-30 23:19:10 -0300

    Update main.js

--- Atividade24/main.js
+++ Atividade24/main.js
@@ -1,20 +1,14 @@
-function dividir(a, b) {
-    let divisao = a / b;
-   
-    if (IsNan(divisao)) {
-       console.log("Divisão deu NaN");
-    } else if (!IsFinite(divisao)) {
-       console.log("Divisão de Infinity");
+   function dividir(a, b) {
+    if (isNaN(a / b)) {
+        console.log("Divisão resultou NaN");
+    } else if (!isFinite(a / b)) {
+        console.log("Divisão de Infinity");
     } else {
-       return divisao;
+        return a / b;
     }
-   }
-   
-   // Testes
-   console.log(dividir(10, 2)); // Retorna 5
-   console.log(dividir(5, 0)); // Retorna "Divisão de Infinity"
-   console.log(dividir(5, "")); // Retorna "Divisão deu NaN"
-   console.log(dividir(0, 0)); // Retorna 0
-   console.log(dividir(null, 5)); // Retorna 0
-   console.log(dividir(5, null)); // Retorna Infinity
-   console.log(dividir(null, null)); // Retorna NaN
+}
+
+console.log(dividir(1, 0));      // Deve imprimir "Divisão de Infinity"
+console.log(dividir(0, 0));      // Deve imprimir "Divisão deu NaN"
+console.log(dividir(1, Infinity)); // Deve imprimir "Divisão de Infinity"
+console.log(dividir(1, 2));      // Deve imprimir o resultado da divisão
