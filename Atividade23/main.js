3d3228c7f370e787a1cc9c847bbb6b7f49876924
Matheus Candido <99218966+mathHenri05@users.noreply.github.com>
2023-11-30 23:09:16 -0300

    Update main.js

--- Atividade23/main.js
+++ Atividade23/main.js
@@ -1,7 +1,12 @@
-function reverso(texto) {
-    var caracteres = texto.split("");
-    var caracteresInvertidos = caracteres.reverse();
-    var textoInvertido = caracteresInvertidos.join("");
-    return textoInvertido;
+function procura(texto) {
+    let contagem = 0;
+    for (let i = 0; i < texto.length; i++) {
+        if (texto[i] === 'A' || texto[i] === 'a') {
+            contagem++;
+        }
+    }
+    return contagem;
 }
-console.log(reverso("A rua sinuosa da colina morena, conduzia até a cena perfeita."));
+console.log(procura("A cara vermelha."));
+console.log(procura("Bruna Caraça, cheia de confiança."));
+console.log(procura("E eu fui o A em meu novo job."));
